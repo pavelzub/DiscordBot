ddf35a7e9f0b3a5b27d5e5855008b2242db0ef02
Alexey Kunitsky <kunitsky.alexey@gmail.com>
2022-05-13 23:27:02 +1000

    Implement bot commands

--- main.py
+++ main.py
@@ -3,22 +3,21 @@ from dotenv import load_dotenv
 import discord
 from discord.ext import commands
 import python_opendota
-from pprint import pprint
 from python_opendota.api import players_api
 from python_opendota.api import heroes_api
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 
-cred = credentials.Certificate("fireToken.json")
-fireApp = firebase_admin.initialize_app(cred, {'databaseURL': 'https://botdis-7d015-default-rtdb.firebaseio.com'}) 
-dotaPlayersRef = db.reference('/DotaPlayers')
-
-def registerDotaPlayer(discordId, dotaId):
-    dotaPlayersRef.update({discordId: dotaId})
-
 load_dotenv()
-bot = discord.Client(intents=discord.Intents.all())
+
+firebase_cred = credentials.Certificate("fireToken.json")
+firebase_app = firebase_admin.initialize_app(firebase_cred, {'databaseURL': 'https://botdis-7d015-default-rtdb.firebaseio.com'}) 
+players_db_ref = db.reference('/DotaPlayers')
+
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix='>', intents=intents)
+
 dota_client = python_opendota.ApiClient(python_opendota.Configuration(host = "http://api.opendota.com/api"))
 player_api = players_api.PlayersApi(dota_client)
 heroes_api = heroes_api.HeroesApi(dota_client)
@@ -26,11 +25,14 @@ heroes_api = heroes_api.HeroesApi(dota_client)
 all_heroes = heroes_api.heroes_get()
 stats = player_api.players_account_id_heroes_get(77315939)
 
-def getHero(name):
+def get_hero(name):
     for hero in all_heroes:
         if hero.localized_name.lower() == name.lower():
             return hero
     return None
+
+def register_dota_player(discord_id, dota_id):
+    players_db_ref.update({ discord_id: dota_id })
 
 @bot.event
 async def on_ready():
@@ -40,17 +42,22 @@ async def on_ready():
 
         guild_count = guild_count + 1
 
-    print("SampleDiscordBot is in " + str(guild_count) + " guilds.")
+@bot.command()
+async def ping(ctx):
+    await ctx.reply('pong')
 
-@bot.event
-async def on_message(message):
-    hero = getHero(message.content)
+@bot.command()
+async def pavel(ctx):
+    await ctx.send('Не смог сделать команды для бота.')
+
+@bot.listen('on_message')
+async def listen_message(message):
+    hero = get_hero(message.content)
     if hero != None:
         for stat in stats:
             if int(stat.hero_id) == hero.id:
                 await message.channel.send('Winrate on {} : {:.2f}%'.format(hero.localized_name, stat.win / stat.games * 100))
 
-# pprint(response)
 
 token = os.getenv('TOKEN', None)
 if token != None:

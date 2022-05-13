5ea6afa61a92bb2aa950721313d13802c01a0e06
Pavel Zubarev <pavelzub1996@gmail.com>
2022-05-13 14:27:44 +1000

    Init commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import discord
+import configparser
+
+bot = discord.Client()
+
+@bot.event
+async def on_ready():
+    guild_count = 0
+    for guild in bot.guilds:
+        print(f"- {guild.id} (name: {guild.name})")
+
+        guild_count = guild_count + 1
+
+    print("SampleDiscordBot is in " + str(guild_count) + " guilds.")
+
+@bot.event
+async def on_message(message):
+    print(message)
+    if message.content == 'test':
+        print('test')
+        await message.channel.send("Dmitriy pidor")
+
+config = configparser.ConfigParser()
+config.read('conf.ini')
+bot.run(config['DEFAULT']['token'])

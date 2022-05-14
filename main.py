de745fdf619016f1de43d6320ddf35a0010fe46d
Alexey Kunitsky <kunitsky.alexey@gmail.com>
2022-05-14 11:19:46 +1000

    Move player hero winrate to separate function

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def get_dota_id(discord_id):
 
 def get_hero(name):
     for hero in all_heroes:
-        if name.lower() in hero.localized_name.lower():
+        if name.lower() == hero.localized_name.lower():
             return hero
     return None
 
@@ -51,6 +51,21 @@ def get_player_stats(dota_player_id):
         print("Exception when calling BenchmarksApi->benchmarks_get: %s\n" % e)
         return None
 
+def get_player_winrate_on_hero(dota_player_id, hero):
+    stats = get_player_stats(dota_player_id)
+    name = get_player_name(dota_player_id)
+    name = name if (name is not None) else 'Somebody'
+
+    if stats is not None:
+        for stat in stats:
+            if int(stat.hero_id) == hero.id:
+                if (stat.games) != 0:
+                    return f'{name}\'s winrate on {hero.localized_name}: {stat.win / stat.games * 100:.2f}%, games {stat.games}'
+                else:
+                    return f'{name} has no games on {hero.localized_name}'
+            
+
+
 def register_dota_player(discord_id, dota_id):
     players_db_ref.update({ discord_id: dota_id })
 
@@ -67,48 +82,34 @@ async def on_ready():
 async def compare(ctx, user_ref, hero_name: str):
     result = re.match(r'<@(\d+)>', user_ref)
     hero = get_hero(hero_name)
-    if result is None or hero is None:
+
+    if (result is None):
         return
 
-    dota_player_first = get_dota_id(ctx.message.author.id)
-    dota_player_second = get_dota_id(result.group(1))
-
-    if dota_player_first is None or dota_player_second is None:
+    if (hero is None):
+        await ctx.reply(f'Хз, что за герой такой: {hero_name}')
         return
 
-    stats_f = get_player_stats(dota_player_first)
-    name_f = get_player_name(dota_player_first)
+    first_player_id = get_dota_id(ctx.message.author.id)
+    second_player_id = get_dota_id(result.group(1))
 
-    stats_s = get_player_stats(dota_player_second)
-    name_s = get_player_name(dota_player_second)
+    if (first_player_id is None):
+        await ctx.reply(f'Так мы не знакомы c тобой, напиши !register', mention_author=True)
 
-    if stats_f is None or name_f is None or stats_s is None or name_s is None:
+    if (second_player_id is None):
+        await ctx.reply(f'Не могу найти дотера {user_ref}')
         return
 
-    result = ''
-    for stat in stats_f:
-        if int(stat.hero_id) == hero.id:
-            result += f'{name_f} winrate on {hero.localized_name}: {stat.win / stat.games * 100:.2f}%, games {stat.games}'
-
-    if result == '':
-        result = f'{name_f} not played on {hero.localized_name}'
-    result += "\n"
-
-    for stat in stats_s:
-        if int(stat.hero_id) == hero.id:
-            result += f'{name_s} winrate on {hero.localized_name}: {stat.win / stat.games * 100:.2f}%, games {stat.games}'
-
-    
-    await ctx.reply(result)
+    await ctx.reply(f'{get_player_winrate_on_hero(first_player_id, hero)}\n{get_player_winrate_on_hero(second_player_id, hero)}')
 
 @bot.command()
 async def register(ctx, dota_player_id):
     name = get_player_name(dota_player_id)
-    if get_dota_id(ctx.message.author.id) is not None:
+    if (get_dota_id(ctx.message.author.id) is not None):
         await ctx.reply(f"За такое надо платить")
         return
     if name != None:
-        await ctx.reply(f"Привет дотер {name}")
+        await ctx.reply(f"Привет, дотер {name}")
         register_dota_player(ctx.message.author.id, dota_player_id)
     else:
         await ctx.reply(f"Кажется кто-то пиздун или закрыл аккаунт")
@@ -119,13 +120,7 @@ async def listen_message(message):
     if hero != None:
         dota_player_id = get_dota_id(message.author.id)
         if dota_player_id is not None:
-            stats = get_player_stats(dota_player_id)
-            name = get_player_name(dota_player_id)
-            name = name if (name is not None) else 'Чей-то'
-            if stats is not None:
-                for stat in stats:
-                    if int(stat.hero_id) == hero.id:
-                        await message.channel.send(f'{name} winrate on {hero.localized_name}: {stat.win / stat.games * 100:.2f}%, games {stat.games}')
+            await message.channel.send(get_player_winrate_on_hero(dota_player_id, hero))
 
 
 token = os.getenv('TOKEN', None)
